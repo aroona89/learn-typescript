b64dd8aa546e1ad7550e24c0449ef7a2373fe6c0
aroona89 <aroonaali899@gmail.com>
2024-04-05 00:54:16 +0500

    CLI number_guessing_game project code Pushed!

---
+++ class-projects/project01_number_guessing_game/index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquier from "inquirer";
+
+// Generate random number from range 1 to 6
+const randomNumber: number = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquier.prompt([
+  {
+    name: "userInput",
+    type: "number",
+    message: "Guess a Number [1-6]:",
+  },
+]);
+
+if (answers.userInput === randomNumber) {
+    console.log("Congratulations! You guessesd a right number.")
+} else {
+    console.log("TRY AGAIN!")
+}
+
+
