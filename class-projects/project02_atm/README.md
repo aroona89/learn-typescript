5dbcd583f1b9838be516068ca81a2a3269474396
aroona89 <aroonaali899@gmail.com>
2024-04-06 00:06:27 +0500

    project02_atm solution code is pushed!

---
+++ class-projects/project02_atm/README.md
@@ -0,0 +1,6 @@
+# ATM
+This somewhat complex TypeScript/Node.js project is a console-based application. When the system starts, the user is prompted with a user pin. After entering the details successfully, the ATM functionalities are unlocked. 
+### Logic Building 
+* user Enters his PIN Number.
+* select option appears offering withdraw and current balance.
+* on clicking withdraw, user has to select one of two modes by entering amount or fast cash.
---
+++ class-projects/project02_atm/index.js
@@ -0,0 +1,71 @@
+import inquirer from "inquirer";
+let currentBalance = 10000; // $ Dollar
+let currentPin = 1234;
+let answer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter Your Pin Number:",
+        type: "number",
+    },
+]);
+// console.log(answer); // return object { pin: 1234 }
+if (answer.pin === currentPin) {
+    console.log("Login Successful!");
+    let operationAnswer = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Select Option:",
+            type: "list",
+            choices: ["Withdraw", "Check Balance"]
+        },
+    ]);
+    if (operationAnswer.operation === "Withdraw") {
+        let withdrawAnswer = await inquirer.prompt([
+            {
+                name: "operation",
+                message: "Select Option:",
+                type: "list",
+                choices: ["Enter Amount", "Fast Cash"]
+            },
+        ]);
+        if (withdrawAnswer.operation == "Enter Amount") {
+            let amountAnswer = await inquirer.prompt([
+                {
+                    name: "amount",
+                    message: "Enter Amount:",
+                    type: "number",
+                },
+            ]);
+            if (amountAnswer.amount <= currentBalance) {
+                currentBalance -= amountAnswer.amount; // assigment operator
+                console.log(`Operation Successful! Your Remaining Balance is ${currentBalance}$.`);
+            }
+            else {
+                console.log("Insufficient Balance!");
+            }
+        }
+        else {
+            let selectAnswer = await inquirer.prompt([
+                {
+                    name: "cash",
+                    message: "Select Amount:",
+                    type: "list",
+                    choices: [1000, 5000, 10000, 20000]
+                },
+            ]);
+            if (selectAnswer.cash <= currentBalance) {
+                currentBalance -= selectAnswer.cash; // assigment operator
+                console.log(`Operation Successful! Your Remaining Balance is ${currentBalance}$.`);
+            }
+            else {
+                console.log("Insufficient Balance!");
+            }
+        }
+    }
+    else {
+        console.log(`Your Current Balance is ${currentBalance}$.`);
+    }
+}
+else {
+    console.log("Enter Correct Pin!");
+}
