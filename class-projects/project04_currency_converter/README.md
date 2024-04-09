8ec4482e0bb92d3b85c7e2060028d1737a167586
aroona89 <aroonaali899@gmail.com>
2024-04-09 21:34:18 +0500

    project currency converter code pushed!

--- class-projects/project04_currency_converter/README.md
+++ class-projects/project04_currency_converter/README.md
@@ -1,4 +1,4 @@
 # Currency Converter
 The TypeScript console app is used to convert currencies: the users enter a certain amount of money in one currency and set the currency they want to check the monetary value of.
 
-While developing the app, the beginners can master variables, objects, algorithms, loops, if statements, arrays and other TypeScript concepts.
+While developing the app, the beginners can master variables, objects, algorithms, arrays and other TypeScript concepts.
--- class-projects/project04_currency_converter/index.js
+++ class-projects/project04_currency_converter/index.js
@@ -1,3 +1,51 @@
 #! /usr/bin/env node
+import inquirer from "inquirer";
 import chalk from "chalk";
-console.log(chalk.bold.blue('Welcome to Simple Currency Converter App!\n'));
+console.log(chalk.bold.yellowBright("Welcome to Simple Currency Converter App!\n"));
+let condition = true;
+const exchangeRate = {
+    USD: 1, // Base currency
+    EUR: 0.91,
+    INR: 74.57,
+    PKR: 280,
+};
+while (condition) {
+    let select = await inquirer.prompt({
+        name: "option",
+        type: "list",
+        message: "Do you want to convert currency?",
+        choices: ["Yes", "No"],
+    });
+    switch (select.option) {
+        case "Yes":
+            let answer = await inquirer.prompt([
+                {
+                    name: "sourceCurrency",
+                    message: "Enter source currency:",
+                    type: "list",
+                    choices: ["PKR", "USD", "INR", "EUR"],
+                },
+                {
+                    name: "targetCurrency",
+                    message: "Enter target currency:",
+                    type: "list",
+                    choices: ["EUR", "INR", "PKR", "USD"],
+                },
+                {
+                    name: "inputAmount",
+                    message: "Enter Amount:",
+                    type: "number",
+                },
+            ]);
+            let sourceCurrencyExchangeRate = exchangeRate[answer.sourceCurrency];
+            let targetCurrencyExchangeRate = exchangeRate[answer.targetCurrency];
+            let calculateConversion = (answer.inputAmount * targetCurrencyExchangeRate) /
+                sourceCurrencyExchangeRate;
+            console.log(chalk.green(`${answer.inputAmount}${answer.sourceCurrency} in ${answer.targetCurrency}: ${calculateConversion.toFixed(2)}`));
+            break;
+        default:
+            console.log(chalk.yellowBright("Thank you for using Currency Converter App!"));
+            condition = false;
+            break;
+    }
+}
