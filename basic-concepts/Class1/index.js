9b88ade5e30653148492ef8432bd81dd428e6fdc
aroona89 <aroonaali899@gmail.com>
2024-04-14 02:34:33 +0500

    template literals

---
+++ basic-concepts/Class1/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var a = "Aroona";
+var b = 23;
+console.log("My name is", a, "and i am", b, "years old."); // method-1
+console.log(`My name is ${a} and i am ${b} years old.`); // method-2 using template literals
---
+++ basic-concepts/Class1/index.ts
@@ -0,0 +1,4 @@
+var a: string = "Aroona";
+var b: number = 23;
+console.log("My name is",a , "and i am", b, "years old.") // method-1
+console.log(`My name is ${a} and i am ${b} years old.`) // method-2 using template literals
