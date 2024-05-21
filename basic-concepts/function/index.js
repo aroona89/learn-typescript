cb8d224af7b8c45b5111e6cc74ef54d1ca533908
aroona89 <aroonaali899@gmail.com>
2024-05-22 02:21:16 +0500

    function as first-class citizen and call back function!

--- basic-concepts/function/index.js
+++ basic-concepts/function/index.js
@@ -87,126 +87,32 @@ optionalParameter("aroona", "ali");
 const defaultParameter = (firstName, lastName = "akbar") => firstName + " " + lastName;
 console.log(defaultParameter("aroona"));
 console.log(defaultParameter("aroona", "ali"));
-// Functions Revision
+// ---------------------------------
+// Functions as first-class citizens & Callback functions
 const addTwoValues = (num1, num2) => {
-    return num1 + num2; //xyz123
+    return num1 + num2;
 };
+// func: (num1: number, num2: number) => number,
+// func is the name of the parameter.
+// (num: number, num5: number) defines the parameters of the func function, which are num1 and num2, both of type number.
+// => number indicates that the func function returns a value of type number.
+// withMultiply: number
+// withMultiply is the name of the parameter.
+// number is the type of the parameter.
 const functionAsParam = (func, withMultiply) => {
-    return func(20, 30) * withMultiply;
+    return func(20, 30) * withMultiply; // func (which is actually addTwoValues) is executed by functionAsParam, making it a callback function.
 };
-const ans = functionAsParam(addTwoValues, 10);
-console.log("🚀 ~ ans:", ans);
-// const returnAValue = (val: string) => {
-//   return val;
-// };
-// const concatString = (
-//   returnAValue: (variable: string) => string,
-//   concat: string
-// ) => {
-//   let returnedValue = returnAValue("Testing");
-//   console.log("🚀 ~ concatString ~ returnedValue:", returnedValue + concat);
-// };
-// concatString(returnAValue, " my program");
-// ---------------------------
-// Loops
-// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// let i = 0;
-// while (i < arr.length) {
-//   console.log(arr[i]);
-//   i++;
-// }
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i] + 10);
-// }
-// let k = 0;
-// do {
-//   console.log("Testing");
-//   k++;
-// } while (k < 0);
+const ans = functionAsParam(addTwoValues, 10); // addTwoValues is passed as an argument to functionAsParam, demonstrating that functions can be treated as values.
+console.log("🚀 product:", ans);
 // ---------------------------------
-/* `Map`, `Filter`, and `Foreach`*/
-// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const arrayMethod = (abc: number, index: number) => {
-//   if ((index + abc) % 2 === 0) {
-//     return "Even";
-//   } else {
-//     return "Odd";
-//   }
-// };
-// let mapReturn = arr.map(arrayMethod);
-// console.log("🚀 ~ mapReturn:", mapReturn);
-// const names = ["Abu Hurairah", "Naveed", "Azhar", "Usman"];
-// const namesUpdate = (nam: string, index: number) => {
-//   if (nam === "Usman") {
-//     return "M. Usman";
-//   }
-//   return nam;
-// };
-// const newNames = names.map(namesUpdate);
-// console.log("🚀 ~ newNames:", newNames);
-// let nums = [-1, 2, 3, -5, 4, -9, 8, -8, -8, -8];
-// const customFunction = (val: number) => {
-//   if (val < 0) {
-//     console.log("Negative");
-//     return "Negative";
-//   } else {
-//     console.log("Positive");
-//     return "Positive";
-//   }
-// };
-// const data = nums.map(customFunction);
-// console.log("🚀 ~ data:", data);
-// const names = [
-//   "Ali",
-//   "Usman",
-//   "Azhar",
-//   "Naveed",
-//   "Abu Hurairah",
-//   "Ramzan",
-//   "Rizwan",
-//   "Sultan",
-//   "Mahad",
-//   "Zain",
-//   "Imran",
-// ];
-// const findName = (name: string) => {
-//   return name.length;
-// };
-// const namesLength = names.map((name: string) => {
-//   return name.length;
-// });
-// console.log("🚀 ~ namesLength:", namesLength);
-// const nums1 = [1, 2, 3, 4, 5, 6, 7];
-// const nums2 = [7, 6, 5, 4, 3, 2, 1];
-// const newArr = nums1.map((val: number, index: number) => {
-//     return val + nums2[index];
-// });
-// console.log("🚀 ~ newArr ~ newArr:", newArr);
+const returnAValue = (val) => {
+    return val;
+};
+const concatString = (returnAValue, // first parameter
+concat // second paarameter
+) => {
+    let returnedValue = returnAValue("Testing"); // Callback function
+    console.log("🚀 concatinaion:", returnedValue + concat);
+};
+concatString(returnAValue, " my program"); // Functions as first-class citizens
 // ---------------------------------
-// Filter
-// const nums1 = [1, 2, 3, 4, 5, 6, 7];
-// const filteredArr = nums1.filter((val: number, index: number) => {
-//   if (index < 2) {
-//     return true;
-//   }
-// });
-// console.log("🚀 ~ filteredArr ~ filteredArr:", filteredArr);
-const names = [
-    "Ali",
-    "Usman",
-    "Azhar",
-    "Naveed",
-    "Abu Hurairah",
-    "Ramzan",
-    "Rizwan",
-    "Sultan",
-    "Mahad",
-    "Zain",
-    "Imran",
-];
-const filteredNames = names.filter((name) => {
-    if (name !== "Abu Hurairah") {
-        return true;
-    }
-});
-console.log("🚀 ~ filteredNames ~ filteredNames:", filteredNames);
